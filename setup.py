0c40df9e2f86f8705c3f3d6bbdb9664c720bdee3
Oliver Salzburg <oliver.salzburg@gmail.com>
2022-02-01 21:02:23 +0100

    chore: Clean up `setup.py`

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
-    name='blockdiag-fences',
-    version='0.0.2',
+    name="blockdiag-fences",
+    version="0.0.2",
     packages=find_packages(),
-    url='https://github.com/oliversalzburg/markdown-blockdiag',
-    license='MIT',
+    url="https://github.com/oliversalzburg/markdown-blockdiag",
+    license="MIT",
     install_requires=[
-        'Markdown',
-        'blockdiag',
-        'seqdiag',
-        'actdiag',
-        'nwdiag',
+        "Markdown",
+        "mkdocs",
+        "superfences",
+        "actdiag",
+        "blockdiag",
+        "nwdiag",
+        "packetdiag",
+        "rackdiag",
+        "seqdiag",
     ],
-    author='Oliver Salzburg',
-    author_email='oliver.salzburg@gmail.com',
-    description='blockdiag extension for Python Markdown',
+    author="Oliver Salzburg",
+    author_email="oliver.salzburg@gmail.com",
+    description="blockdiag extension for Python Markdown",
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown",
 )
